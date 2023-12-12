bedee7dfe3586f85bc632bd5d46bf8c858184fdd
v_wangpengli02 <v_wangpengli02@baidu.com>
2023-12-12 16:50:56 +0800

    fix creator ipcRender sendToWins传递function报错 支持dbbin格式预览

--- viewer/renderer.js
+++ viewer/renderer.js
@@ -9,10 +9,10 @@ ipcRenderer.on('view', (event, path) => {
         getImgBase64(__dirname + "/resource/demo/dragon_tex.png");
     }
 })
-window.onbeforeunload = function(e){
-    Editor.Ipc.sendToWins('close', function (error, answer) { });
+window.onbeforeunload = function (e) {
+    Editor.Ipc.sendToWins('close');
 };
-Editor.Ipc.sendToWins('init', function (error, answer) { });
+Editor.Ipc.sendToWins('init');
 function getImgBase64(path) {
     var base64;
     var img = document.createElement('img');
@@ -39,12 +39,33 @@ function getImgBase64(path) {
 }
 
 function loadRes(path) {
-    let ske_tex_path = path.replace(".png", ".json"), ske_ske_path = path.replace("_tex.png", "_ske.json");
-    fs.readFile(ske_ske_path, 'utf-8', (err, data) => {
-        if (err) {
-            alert(err);
-        } else {
-            dbData[0].data = JSON.parse(data);
+    let ske_tex_path = path.replace(".png", ".json")
+    let ske_ske_path = path.replace("_tex.png", "_ske.json");
+    let isJson = fs.existsSync(ske_ske_path)
+    if (isJson) {
+        fs.readFile(ske_ske_path, 'utf-8', (err, data) => {
+            if (err) {
+                alert(err);
+            } else {
+                dbData[0].data = JSON.parse(data);
+                fs.readFile(ske_tex_path, 'utf-8', (err, value) => {
+                    if (err) {
+                        alert(err);
+                    } else {
+                        dbData[0].textureAtlasDatas.push(JSON.parse(value));
+                        egret.runEgret({ renderMode: "webgl", audioType: 0 });
+                    }
+                });
+            }
+        });
+    } else {
+        //二进制
+        let ske_ske_path = path.replace("_tex.png", "_ske.dbbin");
+        let isBin = fs.existsSync(ske_ske_path)
+        if (isBin) {
+            const binaryData = fs.readFileSync(ske_ske_path);
+            const buffer = new Uint8Array(binaryData).buffer;
+            dbData[0].data = buffer;
             fs.readFile(ske_tex_path, 'utf-8', (err, value) => {
                 if (err) {
                     alert(err);
@@ -53,6 +74,8 @@ function loadRes(path) {
                     egret.runEgret({ renderMode: "webgl", audioType: 0 });
                 }
             });
+        } else {
+            alert("龙骨图片资源路径错误或不存在！");
         }
-    });
+    }
 }
